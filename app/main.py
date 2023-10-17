1926227f2483cc061cc0c5541eab7079b8290b7e
Muhammad Hafizoddin Bin Roslan <>
2023-10-17 23:00:46 +0800

    add and have issue

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from decouple import Config
+
+config = Config('.env')  # point to the local .env file
+
+app = FastAPI()
+
+MONGO_URL = config("MONGO_URL", default="mongo")
+
+client = MongoClient(MONGO_URL, 27017)
+db = client.test_database
+
+@app.get("/")
+def read_root():
+    item = db.test_collection.find_one()
+    return {"Hello": "World", "MongoData": item}
